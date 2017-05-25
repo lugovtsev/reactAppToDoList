7646d64e736d01b9b24bb8c51163361d9b90a69d
lugovtsev <info@lugovtsev.ru>
2017-05-25 16:37:40 +0300

    favorite problem

--- src/App.js
+++ src/App.js
@@ -8,14 +8,15 @@ class App extends Component {
     super(props);
     this.state = {
       list: [
-        {task: 'to do something 1', done: false},
-        {task: 'to do something 2', done: false},
-        {task: 'to do something 3', done: true},
-        {task: 'to do something 4', done: true}
+        {task: 'to do something 1', favorite: false, done: false},
+        {task: 'to do something 2', favorite: true, done: false},
+        {task: 'to do something 3', favorite: false, done: false},
+        {task: 'to do something 4', favorite: false, done: true}
       ]
     }
     this.checkedHandler = this.checkedHandler.bind(this);
     this.addTaskHandler = this.addTaskHandler.bind(this);
+    this.favoriteHandler = this.favoriteHandler.bind(this);
   }
 
   checkedHandler(event) {
@@ -29,7 +30,7 @@ class App extends Component {
   addTaskHandler(event) {
     if (event.keyCode === 13) {
       let newList = this.state.list;
-      newList.push({task: event.target.value, done: false});
+      newList.unshift({task: event.target.value, favorite: false, done: false});
       event.target.value ='';
       this.setState({
         list: newList
@@ -37,12 +38,37 @@ class App extends Component {
     }
   }
 
+  favoriteHandler(event) {
+    console.log(event.target.id);
+    let newList = this.state.list;
+    newList[event.target.id].favorite = (newList[event.target.id].favorite) ? false : true;
+    let movingObj = newList.splice(event.target.id,1);
+    /*function array_values (input) {
+      var tmpArr = []
+      var key = ''
+      for (key in input) {
+        tmpArr[tmpArr.length] = input[key]
+      }
+      return tmpArr
+    }
+    newList = array_values(newList);*/
+    if (newList[event.target.id].favorite) {
+      newList.unshift(movingObj[0]);
+    } else {
+      newList.push(movingObj[0]);
+    }
+
+    this.setState({
+      list: newList
+    })
+  }
+
   render() {
     return (
       <div className="content">
         <h1>MyList <span className="icon-star-empty"></span></h1>
         <TaskAdding pressHandler={this.addTaskHandler}/>
-        <TasksList taskStatus="actual" list={this.state.list} toggleHandler={this.checkedHandler} />
+        <TasksList taskStatus="actual" favoriteHandler={this.favoriteHandler} list={this.state.list} toggleHandler={this.checkedHandler} />
         <div className="controls">
           <span id="show-completed-tasks">Show completed tasks</span>
           <span id="remove-completed-tasks" className="icon-bin" />
--- src/components/TasksListTest.js
+++ src/components/TasksListTest.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const TasksList = ({taskStatus, list, toggleHandler, favoriteHandler}) => {
   if (taskStatus === 'actual') {
-    let actualList = list.map((task, index) => {
+    let actualList = list.map((task) => {
         return (
           (!task.done) ?
-          <div key={index} className="task-item">
-            <input value={index} type="checkbox" onChange={toggleHandler}/>
+          <div key={task.id} className="task-item">
+            <input value={task.id} type="checkbox" onChange={toggleHandler}/>
             <div className="task-item__text">
               <p>{task.task}</p>
-            <span id={index} onClick={favoriteHandler} className={task.favorite ? 'star icon-star-full' : 'star icon-star-empty'}></span>
+            <span id={task.id} onClick={favoriteHandler} className={task.favorite ? 'star icon-star-full' : 'star icon-star-empty'}></span>
             </div>
           </div> : null
         )
@@ -21,11 +21,11 @@ const TasksList = ({taskStatus, list, toggleHandler, favoriteHandler}) => {
       </div>
     )
   } else if (taskStatus === 'done') {
-    let doneList = list.map((task, index) => {
+    let doneList = list.map((task) => {
         return (
           (task.done) ?
-          <div key={index} className="task-item done">
-            <input value={index} type="checkbox" onChange={toggleHandler} checked="checked"/>
+          <div key={task.id} className="task-item done">
+            <input value={task.id} type="checkbox" onChange={toggleHandler} checked="checked"/>
             <div className="task-item__text">
               <p>{task.task}</p>
             </div>
